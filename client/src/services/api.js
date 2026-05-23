5fdd829b0010139afe0ed9e98e99d81e0dd83e67
syamaddala <syamaddala.369@gmail.com>
2026-05-23 13:44:26 +0530

    Connect frontend with backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:
-        process.env.REACT_APP_API_URL ||
-        "http://localhost:5000/api"
+    baseURL: process.env.REACT_APP_API_URL
 })
 
 export default api
