fc72dad2add11ca5a0180cc66ca51936934ba9c8
syamaddala <syamaddala.369@gmail.com>
2026-05-23 11:42:00 +0530

    Add environment variable for API

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL:
+        process.env.REACT_APP_API_URL ||
+        "http://localhost:5000/api"
 })
 
 export default api
